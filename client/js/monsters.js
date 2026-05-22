85032cce5c925db4a2207e101470196b2c22437c
Salangar <grevlansky@gmail.com>
2026-05-22 14:59:13 +0200

    Monster.js Edit

--- client/js/monsters.js
+++ client/js/monsters.js
@@ -3,28 +3,91 @@ const monsters = [
     {
         name: 'Goblin',
         hp: 60,
-        image:
-            'https://images.unsplash.com/photo-1575936123452-b67c3203c357'
+        image: 'assets/bestarium/goblin.png'
+            
     },
 
     {
-        name: 'Farkas',
-        hp: 80,
-        image:
-            'https://images.unsplash.com/photo-1546182990-dffeafbe841d'
+        name: 'Goblin Sámán',
+        hp: 40,
+        image: 'assets/bestarium/goblin_shaman.png'
+            
+    },
+
+    {
+        name: 'Hobgoblin',
+        hp: 100,
+        image: 'assets/bestarium/hobgoblin.png'
+            
+    },
+
+    {
+        name: 'Ghoul',
+        hp: 90,
+        image: 'assets/bestarium/ghoul.png'
+           
     },
 
     {
         name: 'Csontváz',
+        hp: 70,
+        image: 'assets/bestarium/skeleton.png'
+            
+    },
+
+    {
+        name: 'Lidérc',
+        hp: 150,
+        image: 'assets/bestarium/lich.png'
+           
+    },
+
+    {
+        name: 'Élőholt',
         hp: 100,
-        image:
-            'https://images.unsplash.com/photo-1560807707-8cc77767d783'
+        image: 'assets/bestarium/undead.png'
+            
+    },
+
+    {
+        name: 'Erdei Farkas',
+        hp: 50,
+        image: 'assets/bestarium/timber_wolf.png'
+            
+    },
+
+    {
+        name: 'Télfarkas',
+        hp: 80,
+        image: 'assets/bestarium/winter_wolf.png'
+            
+    },
+
+    {
+        name: 'Worg',
+        hp: 115,
+        image: 'assets/bestarium/worg.png'
+            
+    },
+
+    {
+        name: 'Veszett patkány',
+        hp: 25,
+        image: 'assets/bestarium/wild_rat.png'
+           
     },
 
     {
         name: 'Bandita',
         hp: 120,
-        image:
-            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e'
+        image: 'assets/bestarium/bandit.png'
+            
+    },
+
+    {
+        name: 'Hárpia',
+        hp: 250,
+        image: 'assets/bestarium/harpy.png'
+            
     }
 ]
